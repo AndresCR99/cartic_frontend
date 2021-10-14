af4c8760181d0724378ac1ad68790ddcad5cb201
angiegg <angie.garcia@o365.una.edu.co>
2021-10-13 20:48:26 -0500

    modal servicio

--- src/pages/Servicio.js
+++ src/pages/Servicio.js
@@ -1,19 +1,22 @@
 import React, {Fragment} from "react";
-import { Container } from "react-bootstrap";
-
+import  {useState} from 'react';
+import { Container,Button } from "react-bootstrap";
 import PanelServicio from "../components/Servicio/PanelServicio";
+import ModalServicio from "../components/Servicio/ModalServicio";
 
 
 
 
 const Servicio = () => {
-
-    return(
+    const [modalShow, setModalShow] = useState(false);
+    <ModalServicio show={modalShow} onHide={() => setModalShow(false)} />
+   return(
 
         <Fragment>
             <Container>
             <PanelServicio/>
-              
+            <ModalServicio/>
+            <Button  className="m-4"variant="primary" onClick={() => setModalShow(true)}>+ Servicio</Button>
             </Container>
         </Fragment>
     )
