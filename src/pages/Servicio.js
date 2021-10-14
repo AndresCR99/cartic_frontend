6f8738919a957e4108bf5080375a1679a01a9194
angiegg <angie.garcia@o365.una.edu.co>
2021-10-13 19:59:10 -0500

    rutas y panl servicio

---
+++ src/pages/Servicio.js
@@ -0,0 +1,22 @@
+import React, {Fragment} from "react";
+import { Container } from "react-bootstrap";
+
+import PanelServicio from "../components/Servicio/PanelServicio";
+
+
+
+
+const Servicio = () => {
+
+    return(
+
+        <Fragment>
+            <Container>
+            <PanelServicio/>
+              
+            </Container>
+        </Fragment>
+    )
+
+}
+export default Servicio;
