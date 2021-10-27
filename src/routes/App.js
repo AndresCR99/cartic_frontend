1f369ab289323750c100bc7107408212bf0561af
andresCR99 <carlos.castano@o365.unab.edu.co>
2021-10-26 23:43:16 -0500

    not used apps.js 6:8, tablatecnico container 2:17

--- src/routes/App.js
+++ src/routes/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "../components/styles/App.css";
 import Tecnico from "../pages/Tecnico";
 import E404 from "../pages/errors/E404";
-import Header from "../components/Header";
+// import Header from "../components/Header";
 import Footer from "../components/Footer";
 import RegistroCli from "../pages/RegistroCli";
 import Servicio from "../pages/Servicio";
